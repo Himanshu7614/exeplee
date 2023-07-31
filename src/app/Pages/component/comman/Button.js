13fcb72ba2a77d2e7132d7fce177a5b4862de15c
Himanshu <86042253+Himanshu7614@users.noreply.github.com>
2023-07-31 09:04:28 +0530

    Update Button.js

--- src/app/Pages/component/comman/Button.js
+++ src/app/Pages/component/comman/Button.js
@@ -2,7 +2,7 @@ function Button({buttonname, clickMe = null, className=null}) {
     return (
       <>
         <button className={`px-4 py-1.5 rounded-full flex items-center w-auto bg-gradient-to-l from-blue-200 via-blue-400 to-blue-700 text-white text-center font-bold hover:px-8 duration-300 ${className}`} onClick={clickMe}> 
-          {buttonname};
+          {buttonname}
         </button>
         
       </>
